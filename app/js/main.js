776a2e6206b2533beb0c28078ed9e54c4aeb6e20
AndIReyf <andrey.prudius@hotmail.com>
2020-01-18 21:39:54 +0100

    Added header

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,8 @@ $(function () {
 
   new WOW().init();
 
-  $('.header__burger').click(function () {
-    $('.header__burger, .header__menu').toggleClass('active');
+  $('.header-burger').click(function () {
+    $('.header-burger, .header-menu').toggleClass('active');
     $('html, body').toggleClass('lock');
   });
 
